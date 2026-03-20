0b2f823862e0c2d66317350efb75015f9990cda6
Luisa Peralta <lperaltav@unal.edu.co>
2026-03-20 16:14:59 -0500

    feat(se edito la clase de estudiante y profesor)

--- src/main/java/TareaSia/Estudiante.java
+++ src/main/java/TareaSia/Estudiante.java
@@ -8,6 +8,37 @@ package TareaSia;
  *
  * @author Brayan Ramirez
  */
+
 public class Estudiante {
+    //Atributos 
+    String nombre;
+    long cedula_tarjeta;
+    String correo;
+    String programa;
+    int semestre_cursado;
+    
+    //Metodos 
+    public Estudiante(String nombre, String programa){
+        this.nombre = nombre;
+        this.programa = programa;
+    }
+    public Estudiante(String nombre, String programa,long cedula_tarjeta,int semestre_cursado,String correo){
+        this.nombre = nombre;
+        this.programa = programa;
+        this.semestre_cursado = semestre_cursado;
+        this.correo = correo;
+        this.cedula_tarjeta = cedula_tarjeta;
+        
+    }
+    public void cursar(){
+        System.out.println("Me llamo" +nombre+ "estoy cursando" +semestre_cursado+ "semestre");
+    }
+    public void cursar(int edad){
+         System.out.println("Me llamo" +nombre+ "tengo" +edad+ "años y estoy en el progrma de" +programa);
+    }
     
 }
+
+
+    
+
--- src/main/java/TareaSia/Profesor.java
+++ src/main/java/TareaSia/Profesor.java
@@ -8,6 +8,37 @@ package TareaSia;
  *
  * @author Brayan Ramirez
  */
-public class Profesor {
+
+
+public class Profesor{
+    
+    //Atributos 
+    String nombre;
+    long cedula;
+    String correo;
+    String departamento;
+    long celular;
+  
+    
+    //Metodos 
+    public Profesor(String nombre, String departamento){
+        this.nombre = nombre;
+        this.departamento = departamento;
+    }
+    public Profesor(String nombre, String departamento,long cedula,String correo, long celular){
+        this.nombre = nombre;
+        this.departamento = departamento;
+        this.celular = celular;
+        this.correo = correo;
+        this.cedula = cedula;
+        
+    }
+    public void dictar(){
+        System.out.println("Me llamo" +nombre+ ",soy profesor del departamento" +departamento );
+    }
+    public void dictar(String curso){
+         System.out.println("Me llamo" +nombre+ "dicto el curso de" +curso+ 
+                 "y me pueden contactar atravez de mi correo"  +correo );
+    }
     
 }
